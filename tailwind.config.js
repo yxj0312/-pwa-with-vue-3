92c3ae90536ed3b4f5e561e6972f985d08d51694
yxj0312 <yanxiajun@hotmail.com>
2022-05-03 23:43:47 +0200

    add tailwind settings

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge:['./public/index.html','./src/**/*.{vue,js}'],
+  darkMode: false,
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
