8e2bd4d6577287d31eab3c5f31d686cbaabb2a7b
yxj0312 <yanxiajun@hotmail.com>
2022-05-06 23:40:36 +0200

    add add method

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="flex w-screen h-screen text-gray-700">
-        <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
-            <!-- Siderbar -->
-        </div>
-        <div class="flex flex-col flex-grow">
-            <!-- Main Content -->
-            <div class="flex flex-col flex-grow overflow-auto">
-                <editor-content :editor="editor" />
-            </div>
-        </div>
+  <div class="flex w-screen h-screen text-gray-700">
+    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
+      <!-- Siderbar -->
     </div>
+    <div class="flex flex-col flex-grow">
+      <!-- Main Content -->
+      <div class="flex flex-col flex-grow overflow-auto">
+        <editor-content :editor="editor" />
+      </div>
+      <div class="h-16 bg-gray-100 border-t border-gray-300 text-right">
+        <button @click="saveNote()" class="border-t bg-blue-400 text-white">Save Note</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {useEditor, EditorContent} from '@tiptap/vue-3'
+import { useEditor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
 
 const editor = useEditor({
@@ -22,39 +25,60 @@ const editor = useEditor({
     StarterKit,
   ],
   editorProps: {
-      attributes: {
-          class: "prose my-6 mx-auto focus:outline-one"
-      }
+    attributes: {
+      class: "prose my-6 mx-auto focus:outline-one"
+    }
   }
 })
 
 let database = null
 
 const getDatabase = async () => {
-      return new Promise((resolve, reject) => {
-        if (database) {
-          resolve(database)
-        }
-        let request = window.indexedDB.open('notes', 2)
-        request.onerror = event => {
-          console.error('ERROR: Unable to open database', event)
-          reject('Error')
-        }
-        request.onsuccess = event => {
-          database = event.target.result
-          resolve(database)
-        }
-
-        request.onupgradeneeded = event => {
-          let database = event.target.result
-          database.createObjectStore('notes', {
-            autoIncrement: true,
-            keyPath: 'id'
-          })
-        }
-      })
+  return new Promise((resolve, reject) => {
+    if (database) {
+      resolve(database)
+    }
+    let request = window.indexedDB.open('notes', 2)
+    request.onerror = event => {
+      console.error('ERROR: Unable to open database', event)
+      reject('Error')
+    }
+    request.onsuccess = event => {
+      database = event.target.result
+      resolve(database)
     }
 
-    getDatabase();
+    request.onupgradeneeded = event => {
+      let database = event.target.result
+      database.createObjectStore('notes', {
+        autoIncrement: true,
+        keyPath: 'id'
+      })
+    }
+  })
+}
+
+const saveNote = async () => {
+  return new Promise((resolve, reject) => {
+    let transaction = database.transaction('notes', 'readwrite')
+    transaction.oncomplete = e => {
+      resolve();
+      console.log(e)
+    }
+    transaction.onerror = e => {
+      console.log(e.target.result)
+      reject('Error')
+    }
+
+    let now = new Date();
+    transaction.objectStore('notes').add({
+      content: editor.getHTML(),
+      created: now.getTime()
+    })
+  })
+  
+}
+
+getDatabase();
 
 </script>
