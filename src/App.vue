22cd391a915644fd01eddc30c5ffda2bbb592a99
yxj0312 <yanxiajun@hotmail.com>
2022-05-16 22:50:02 +0200

    update editor

--- src/App.vue
+++ src/App.vue
@@ -33,8 +33,8 @@
         </div>
       </div>
     </div>
-    <div class="flex flex-col flex-grow">
-      <!-- Main Content -->
+    <div class="flex flex-col flex-grow" v-if="Object.keys(activeNote).length">
+      <!-- Main Content - Editor -->
       <div class="flex flex-col flex-grow overflow-auto">
         <editor-content :editor="editor" />
       </div>
@@ -42,6 +42,22 @@
         <button @click="saveNote()" class="border-t bg-blue-400 text-white">
           Save Note
         </button>
+      </div>
+    </div>
+    <div class="flex flex-col flex-grow" v-else>
+      <!-- Main Content - Notes List -->
+      <div class="flex flex-col flex-grow overflow-auto">
+        <div v-for="note in notes" :key="note.created">
+          <div class="flex px-4 pt-3 pb-4">
+            <div class="prose my-2 mx-auto">
+              <div>
+                <span class="ml-1 text-xs text-gray-500">Created on {{ new Date(note.created).toLocaleString() }}</span>
+              </div>
+              <div v-html="note.content"></div>
+            </div>
+          </div>
+          <hr class="w-full">
+        </div>
       </div>
     </div>
   </div>
