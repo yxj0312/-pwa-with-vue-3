389354f3b675f466ae6d46d31d44592b339bc18b
yxj0312 <yanxiajun@hotmail.com>
2022-05-19 23:06:28 +0200

    make some changes to config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     pwa: {
+        // workboxPluginMode: "InjectManifest",
         name: 'Todo Mastery',
         workboxOptions: {
             skipWaiting: true
+            // swSrc: "./src/service-worker.js"
         }
-
     }
 }
