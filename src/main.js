70f4a49c403d817c3dd0b996ca4648dc61bf6006
yxj0312 <yanxiajun@hotmail.com>
2022-05-21 22:16:26 +0200

    add vuex config

--- src/main.js
+++ src/main.js
@@ -2,5 +2,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './app.css'
 import './registerServiceWorker'
+import {createStore} from 'vuex'
 
-createApp(App).mount('#app')
+const store = createStore({
+    state() {
+        return {
+
+        }
+    },
+    mutations: {
+        
+    },
+    actions: {
+
+    }
+})
+
+const app = createApp(App)
+
+app.use(store)
+
+app.mount('#app')
